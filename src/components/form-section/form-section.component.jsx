a06883709b9a88633bc4be565249c91d7a3fed77
Mark9Mbugua <mbuguamark@gmail.com>
2021-05-09 12:22:10 +0300

    Created a form using draft-js-plugin

--- src/components/form-section/form-section.component.jsx
+++ src/components/form-section/form-section.component.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState, useRef } from 'react';
+import Editor, { createEditorStateWithText } from '@draft-js-plugins/editor';
+import createToolbarPlugin from '@draft-js-plugins/static-toolbar';
+import editorStyles from './form-section.module.scss';
+
+const staticToolbarPlugin = createToolbarPlugin();
+const { Toolbar } = staticToolbarPlugin;
+const plugins = [staticToolbarPlugin];
+const text = '';
 
 const FormSection = () => {
+    const [editorState, setEditorState] = useState(() =>createEditorStateWithText(text));
+    
+    let editor = useRef(null);
+
+
+    const onChange = (editorState) => {
+        setEditorState(editorState);
+    };
+
+    const focus = () => {
+        editor.focus();
+    };
+
     return (
         <div>
-            <h1>Create Article Form</h1>
+            <div className={editorStyles.editor} onClick={focus}>
+                <div className={editorStyles.toolbar}>
+                    <Toolbar/>
+                </div>
+                <Editor
+                    editorState={editorState}
+                    onChange={onChange}
+                    plugins={plugins}
+                    ref={el => editor = el}
+                />
+            </div>
         </div>
-    )
+    );
 }
 
 export default FormSection
